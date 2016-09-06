9191c97a1a46acb9b5f90b39c9165505aaad63a9
yaoelvon <yaoelvon@gmail.com>
2016-09-06 20:55:56 +0800

    finish second task

--- taxi_meter.py
+++ taxi_meter.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 
-class TaxiMeter():
+class TaxiMeter(object):
     def __init__(self):
         self.cash = 0
         self.start_time = None
@@ -11,4 +11,8 @@ class TaxiMeter():
         self.start_time = float(start_time)
 
     def receive(self, current_time, miles):
-        self.cash = 14
+        print "miles: {0}".format(miles)
+        if miles > 3:
+            self.cash = 14 + (miles - 3) * 2.4
+        else:
+            self.cash = 14
